7c2db983442f42dfb248c24360b3b312969839ea
Avutzhan Dautov <dautov92@list.ru>
2020-08-27 15:44:15 +0600

    Extracting to View Composers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \View::share('channels', \App\Channel::all());
+//        \View::composer('*', function ($view) {
+//            $view->with('channels', \App\Channel::all());
+//        });
+//        \View::composer('threads.create', function ($view) {
+//            $view->with('channels', \App\Channel::all());
+//        });
     }
 
     /**

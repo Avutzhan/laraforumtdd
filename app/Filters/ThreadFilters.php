a5c617ba70a9ccca571be0f700d6743627e6643e
Avutzhan Dautov <dautov92@list.ru>
2020-08-27 18:00:34 +0600

    A Lesson in Refactoring

---
+++ app/Filters/ThreadFilters.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filters;
+
+use Illuminate\Http\Request;
+
+class ThreadFilters extends Filters
+{
+    protected $filters = ['by'];
+    /**
+     * @param $username
+     * @return mixed
+     */
+    public function by($username)
+    {
+        $user = \App\User::where('name', $username)->firstOrFail();
+
+        return $this->builder->where('user_id', $user->id);
+    }
+}
--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Channel;
+use App\Filters\ThreadFilters;
 use App\Thread;
 use Illuminate\Http\Request;
 
@@ -17,20 +18,10 @@ class ThreadsController extends Controller
      * @param null $channelId
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function index(Channel $channel)
+    public function index(Channel $channel, ThreadFilters $filters)
     {
-        if ($channel->exists) {
-            $threads = $channel->threads()->latest();
-        } else {
-            $threads = Thread::latest();
-        }
+        $threads = $this->getThreads($channel, $filters);
 
-        if ($username = request('by')) {
-            $user = \App\User::where('name', $username)->firstOrFail();
-            $threads->where('user_id', $user->id);
-        }
-
-        $threads = $threads->get();
 
         return view('threads.index', compact('threads'));
     }
@@ -111,5 +102,22 @@ class ThreadsController extends Controller
     public function destroy(Thread $thread)
     {
         //
+    }
+
+    /**
+     * @param Channel $channel
+     * @param ThreadFilters $filters
+     * @return mixed
+     */
+    public function getThreads(Channel $channel, ThreadFilters $filters)
+    {
+        $threads = Thread::latest()->filter($filters);
+
+        if ($channel->exists) {
+            $threads->where('channel_id', $channel->id);
+        }
+
+        $threads = $threads->get();
+        return $threads;
     }
 }

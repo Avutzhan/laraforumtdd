6e30a53e5191ff72e8b501448ee64267da0f81c0
Avutzhan Dautov <dautov92@list.ru>
2020-09-03 14:00:18 +0600

    A More Data-centric Approach

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,62 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <div class="level">
-                            <span>
-                                <a href="/profiles/{{ $thread->creator->name }}">{{ $thread->creator->name }}</a> posted:
-                            {{ $thread->title }}
-                            </span>
+    <thread-view :initial-replies-count="{{ $thread->replies_count }}" inline-template>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <div class="level">
+                                <span>
+                                    <a href="/profiles/{{ $thread->creator->name }}">{{ $thread->creator->name }}</a> posted:
+                                {{ $thread->title }}
+                                </span>
 
-                            @can ('update', $thread)
-                                <form method="POST" action="{{ $thread->path() }}">
-                                    {{ csrf_field() }}
-                                    {{ method_field('DELETE') }}
+                                @can ('update', $thread)
+                                    <form method="POST" action="{{ $thread->path() }}">
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
 
-                                    <button type="submit" class="btn btn-link">Delete Thread</button>
-                                </form>
-                            @endcan
+                                        <button type="submit" class="btn btn-link">Delete Thread</button>
+                                    </form>
+                                @endcan
+                            </div>
+                        </div>
+
+                        <div class="panel-body">
+                            {{ $thread->body }}
                         </div>
                     </div>
 
-                    <div class="panel-body">
-                        {{ $thread->body }}
-                    </div>
+                    <replies :data="{{ $thread->replies }}" @removed="repliesCount--"></replies>
+
+    {{--                {{ $replies->links() }}--}}
+
+                    @if(auth()->check())
+                        <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <textarea name="body" id="body" class="form-control" placeholder="Say!" rows="5"></textarea>
+
+                                <button type="submit" class="btn btn-default">Post</button>
+                            </div>
+                        </form>
+                    @else
+                        <p class="text-center"><a href="{{ route('login') }}">Sign in</a> to participate</p>
+                    @endif
                 </div>
 
-                @foreach($replies as $reply)
-                    @include('threads.reply')
-                @endforeach
-
-                {{ $replies->links() }}
-
-                @if(auth()->check())
-                    <form method="POST" action="{{ $thread->path() . '/replies' }}">
-                        {{ csrf_field() }}
-                        <div class="form-group">
-                            <textarea name="body" id="body" class="form-control" placeholder="Say!" rows="5"></textarea>
-
-                            <button type="submit" class="btn btn-default">Post</button>
+                <div class="col-md-4">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            Tjis thread was published {{ $thread->created_at->diffForHumans() }} by
+                            <a href="#">{{ $thread->creator->name }}</a>, and currently has
+                            <span v-text="repliesCount"></span> {{ str_plural('comment', $thread->repies_count) }}
                         </div>
-                    </form>
-                @else
-                    <p class="text-center"><a href="{{ route('login') }}">Sign in</a> to participate</p>
-                @endif
-            </div>
-
-            <div class="col-md-4">
-                <div class="panel panel-default">
-                    <div class="panel-body">
-                        Tjis thread was published {{ $thread->created_at->diffForHumans() }} by
-                        <a href="#">{{ $thread->creator->name }}</a>, and currently has
-                        {{ $thread->replies_count }} {{ str_plural('comment', $thread->repies_count) }}
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </thread-view>
 @endsection

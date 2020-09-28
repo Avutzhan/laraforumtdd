e03563604992a4ba1de337493ea6c42ce975556a
Avutzhan Dautov <dautov92@list.ru>
2020-09-28 12:26:48 +0600

    Confusing Errors and Solutions

--- database/migrations/2020_09_05_062821_create_thread_subscriptions_table.php
+++ database/migrations/2020_09_05_062821_create_thread_subscriptions_table.php
@@ -24,7 +24,6 @@ class CreateThreadSubscriptionsTable extends Migration
                 ->references('id')
                 ->on('threads')
                 ->onDelete('cascade');
-
         });
     }
 

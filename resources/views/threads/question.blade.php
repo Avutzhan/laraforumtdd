b7c47b1a1e3f54d4b163d5fc367cfce478e32930
Avutzhan Dautov <dautov92@list.ru>
2020-10-07 00:49:15 +0600

    A Thread Can Be Updated: Part 3

--- resources/views/threads/question.blade.php
+++ resources/views/threads/question.blade.php
@@ -1,28 +1,21 @@
 <div class="panel panel-default" v-if="editing">
     <div class="panel-heading">
         <div class="level">
-            <input type="text" value="{{ $thread->title }}" class="form-control">
-
-            <span>
-                <a href="/profiles/{{ $thread->creator->name }}">{{ $thread->creator->name }}</a> posted:
-            {{ $thread->title }}
-            </span>
-
-
+            <input type="text" class="form-control" v-model="form.title">
         </div>
     </div>
 
     <div class="panel-body">
         <div class="form-group">
-            <textarea class="form-control" rows="10">{{ $thread->body }}</textarea>
+            <textarea class="form-control" rows="10" v-model="form.body"></textarea>
         </div>
     </div>
 
     <div class="panel-footer">
         <div class="level">
             <button class="btn btn-xs level-item" @click="editing = true" v-show="! editing">Edit</button>
-            <button class="btn btn-primary btn-xs level-item" @click="">Update</button>
-            <button class="btn btn-xs level-item" @click="editing = false">Cancel</button>
+            <button class="btn btn-primary btn-xs level-item" @click="update">Update</button>
+            <button class="btn btn-xs level-item" @click="resetForm">Cancel</button>
 
             @can ('update', $thread)
                 <form method="POST" action="{{ $thread->path() }}" class="ml-a">
@@ -40,21 +33,22 @@
 <div class="panel panel-default" v-else>
     <div class="panel-heading">
         <div class="level">
-            <img src="{{ $thread->creator->avatar_path }}" width="25" height="25" alt="{{ $thread->creator->name }}" class="mr-1">
+            <img src="{{ $thread->creator->avatar_path }}"
+                 width="25"
+                 height="25"
+                 alt="{{ $thread->creator->name }}"
+                 class="mr-1">
 
-            <span>
-                <a href="/profiles/{{ $thread->creator->name }}">{{ $thread->creator->name }}</a> posted:
-            {{ $thread->title }}
+            <span class="flex">
+                <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted: <span v-text="title"></span>
             </span>
 
         </div>
     </div>
 
-    <div class="panel-body">
-        {{ $thread->body }}
-    </div>
+    <div class="panel-body" v-text="body"></div>
 
-    <div class="panel-footer">
+    <div class="panel-footer" v-if="authorize('owns', thread)">
         <button class="btn btn-xs" @click="editing = true">Edit</button>
     </div>
 </div>

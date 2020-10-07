578166e30255bc0b7b787d591d48708eaade26b3
Avutzhan Dautov <dautov92@list.ru>
2020-10-07 13:15:45 +0600

    First Class Search: Implementation

---
+++ tests/Feature/SearchTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Thread;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SearchTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_search_threads()
+    {
+        config(['scout.driver' => 'algolia']);
+        $search = 'foobar';
+
+        create('App\Thread', [], 2);
+        create('App\Thread', ['body' => "A thread with the {$search} term."], 2);
+
+        do {
+            sleep(.25);
+            $result = $this->getJson("/threads/search?q={$search}")->json()['data'];
+        } while (empty($result));
+
+        $this->assertCount(2, $result);
+
+        Thread::latest()->take(4)->unsearchable();
+
+    }
+}
